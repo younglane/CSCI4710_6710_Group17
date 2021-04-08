7aaeea34543199b55afeccde599785fef1cc0bd7
adamwainright <wainrighta16@students.ecu.edu>
2021-04-08 15:46:03 -0400

    got db to load in html

--- main.py
+++ main.py
@@ -58,11 +58,14 @@ class Survey(db.Model):
         self.meaningFactor = meaningFactor
         self.job = job
         
-    def repr(self):
+    #def __getitem__(self):
+        #return self.id_num,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job
+        
+    def __repr__(self):
         return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.id_num,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job)
 
-    def iter(self):
-        return iter([self.id_num,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job])
+    def __iter__(self):
+        return iter([self.index,self.country, self.age,self.gender,self.fearFactor,self.anxiousFactor,self.angerFactor,self.happyFactor,self.sadFactor,self.emotionFactor,self.whyFactor,self.meaningFactor, self.job])
 
 
 
@@ -78,45 +81,32 @@ with open('t.csv', 'r') as csvfile:
         db.session.add(response)
         db.session.commit()
 
-try:
-    for row in Survey.query.all():
-        print(row.index,row.country,row.age,row.gender,row.fearFactor,row.anxiousFactor,row.angerFactor,row.happyFactor,row.sadFactor,row.emotionFactor,row.whyFactor,row.meaningFactor,row.job)
-except:
-    print("error")
-
-    
 
 
+#table = Survey.query.all()
+#def object_as_dict(obj):
+    #return {c.key: getattr(obj, c.key)
+            #for c in inspect(obj).mapper.column_attrs}
 
 
+#try:
+    #user_dict = object_as_dict(table)
+    #for row in table:
+        #print(object_as_dict(row))
+    #for row in user_data:
+        #print(row._asdict())
+        #user_data.append(row.index,row.country,row.age,row.gender,row.fearFactor,row.anxiousFactor,row.angerFactor,row.happyFactor,row.sadFactor,row.emotionFactor,row.whyFactor,row.meaningFactor,row.job)
+#except:
+    #print("error")
 
-    
-
-# evil gloabl variable...
-# the data should be obtained from your db
-
-
-sample_data = {
-	'user_data':[
-		[1,"USA",70.0,"Male",2,2,1,2,2,"anticipation of whats going to happen next","Lot's of predictions from differnet resources","Family,Psychologist"],
-		[2,"Switzerland",25.0,"Female",3,4,3,4,4,"A mix of awe and anxiety. Awe at how wonderful of a challenge this is for our world to tackle together (especially on issues that have plagued us for decades) and anxiety on how much we may not be thinking about those who are most vulnerable","Reading thought leadership articles and social media","Reading,Global Public Servant (WEF)"],
-		[3,"USA",26.0,"Female",3,3,1,4,4,"A mix of happy to be safe and home and sad for people who aren't","Seeing what's happening in the news","Family,Student"],
-		[4,"USA",11.0,"Male",2,1,5,1,3,"Anger,No sports" ,"Family,Student"],
-		[5,"USA",28.0,"Male",4,3,4,1,4,"Anger","a system that cares about profit more than general wellbeing","trying to help","reporter"],
-		[6,"USA",24.0,"Female",4,4,5,1,4,"Anger","The US federal government" ,"Friends","Graduate student"],
-		[7,"USA",21.0,"Female",4,3,5,3,2,"Anger","People are not being responsible in doing their part in staying in doors. It is causing a major spread in our environment. Due to this spread, I am not allowed to be outdoors or stay in my apartment in Austin. I am very active outside, so this is a challenge for me to stay inside. ","Being alone,Student"],
-		[8,"USA",22.0,"Male",3,4,5,2,1,"Anger","Inaction of the people and government","Friends","Janitor"],
-		[9,"USA",39.0,"Male",2,2,4,1,2,"Anger","Impacts on day-to-day life","Exercising","Software Engineer"],
-		[10,"USA",78.0,"Male",5,5,5,1,1,"Anger","National leadership incompetence","Family","Executive retired"]		
-	]
-}
+column_names = ["index","What country do you live in?","How old are you?","What is your gender?","To what extent do you feel FEAR due to the coronavirus?","To what extent do you feel ANXIOUS due to the coronavirus?","To what extent do you feel ANGRY due to the coronavirus?","To what extent do you feel HAPPY due to the coronavirus?","To what extent do you feel SAD due to the coronavirus?","Which emotion is having the biggest impact on you?","What makes you feel that way?","What brings you the most meaning during the coronavirus outbreak?","What is your occupation?"]
 
 
 
 @app.route('/')
 def index():
-    labels = util.cluster_user_data(sample_data['user_data'])
-    return render_template('index.html', labels_html=labels, column_html=column_names, data_html=sample_data['user_data'])
+    #labels = util.cluster_user_data(Survey.query.all())
+    return render_template('index.html', column_html=column_names, data_html=Survey.query.all())
 
 
 if __name__ == '__main__':
